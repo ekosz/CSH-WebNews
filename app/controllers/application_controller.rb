daf7f4d4f8a83aa80d914818814b608e9c111f7b
Alex Grant <grantovich@csh.rit.edu>
2011-07-28 15:04:54 -0400

    Add totally untested WebAuthOptional handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   require 'net/nntp'
   protect_from_forgery
-  before_filter :get_current_user
+  before_filter :authenticate, :get_user
   
   private
   
-    def get_current_user
+    def authenticate
+      if not ENV['WEBAUTH_USER']
+        render :file => "#{RAILS_ROOT}/public/auth.html"
+      end
+    end
+  
+    def get_user
       @current_user = User.find_by_username(ENV['WEBAUTH_USER'])
       @current_user.touch if not @current_user.nil?
     end
