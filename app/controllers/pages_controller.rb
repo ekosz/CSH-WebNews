e739b21934b4554d23d415800cb1564ae5817579
Alex Grant <grantovich@csh.rit.edu>
2011-07-31 18:00:46 -0400

    Pick up CN changes on full reload

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   before_filter :get_newsgroups, :only => :home
 
   def home
+    @current_user.real_name = ENV['WEBAUTH_LDAP_CN']
+    @current_user.save!
   end
   
   def new_user
